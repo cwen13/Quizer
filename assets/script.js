7159ef43654903e2221a6b43c0e12066dfa73f49
cwen13 <cwenrich13@gmail.com>
2022-11-23 23:06:43 -0500

    set up HTML and started JS

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// JS file for questions and answers
+//--------JSON-Questions-&-Answers------------>
+const QnAs = {
+  "questions": [
+    {"question0": "String for a question0?",
+      "Answers": ["A","B","C","D"],
+      "Answer": "A"},
+    {"question1": "String for a question1?",
+      "Answers": ["A","B","C","D"],
+      "Answer": "B"},
+    {"question2": "String for a question3?",
+      "Answers": ["A","B","C","D"],
+      "Answer": "C"}]}
+//-------------------------------------------->
+
